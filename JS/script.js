28318d5acbbb896431346da2930968997ec9643e
akramalex <info@quick4you.net>
2024-04-19 13:13:09 +0100

    comment

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("h2").addClass("underline"); // underlines all <h2> elements
+    $("ul").addClass("border"); // adds a border to all list
+    
+});
+
+// $("h2").removeClass("underline"); // underlines all <h2> elements
+// $("ul").removeClass("border"); // adds a border to all list
+
+// $('tr:odd').addClass('odd');
+// $('tr:even').addClass('even');
